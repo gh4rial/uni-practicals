9fbf3e550ccfd0bfe584a842d69ddbc63cc0ae28
Govind Hari <173696360+gh4rial@users.noreply.github.com>
2026-04-08 21:12:14 +0530

    Improved testing for sorts and searching in WIS

--- daa/test_sorts.cpp
+++ daa/test_sorts.cpp
@@ -6,63 +6,103 @@
 
 #include "sorts.hpp"
 
+#define RUN_COUNT 100 // number of "runs"
+#define RUN_SIZE  10  // number of "input instances" in each run
+
+int merge_sort_copy(int A[], int size)
+{
+    int *B = new int[size];
+    for (int i = 0; i < size; i++) {
+        B[i] = A[i];
+    }
+
+    return merge_sort(B, size);
+}
+
+int heap_sort_copy(int A[], int size)
+{
+    int *B = new int[size];
+    for (int i = 0; i < size; i++) {
+        B[i] = A[i];
+    }
+
+    return heap_sort(B, size);
+}
+
+int quick_sort_copy(int A[], int size)
+{
+    int *B = new int[size];
+    for (int i = 0; i < size; i++) {
+        B[i] = A[i];
+    }
+
+    return quick_sort(B, size);
+}
+
+int insertion_sort_copy(int A[], int size)
+{
+    int *B = new int[size];
+    for (int i = 0; i < size; i++) {
+        B[i] = A[i];
+    }
+
+    return insertion_sort(B, size);
+}
+
+void generate_data(int A[], int size)
+{
+    for (int i = 0; i < size; i++) {
+        A[i] = std::rand() % 1000 + 1; // 1 to 1000
+    }
+}
+
 int main()
 {
     // seeding with current time for random value generation
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    std::srand((unsigned int)std::time(NULL));
 
     std::ofstream csv("sorts.csv");
+
     if (!csv) {
-        std::cout << "Error: could not open file\n";
+        std::cout << "Error: could not open csv file\n";
         return 1;
     }
 
     // columns
-    csv << "size,insertion,merge,heap,quick,nlogn\n";
+    csv << "size,insertion,merge,heap,quick,n log n\n";
 
-    int input_sizes[10];
-    int *inputs[10];
+    long avg_ins, avg_heap, avg_quick, avg_merge;
 
-    // populate arrays
-    for (int i = 0; i < 10; i++) {
-        input_sizes[i] = std::rand() % 971 + 30; // 30 to 1000
-        inputs[i] = new int[input_sizes[i]];
+    for (int i = 0; i < RUN_COUNT; i++) {
+        avg_ins = avg_heap = avg_quick = avg_merge = 0;
 
-        for (int j = 0; j < input_sizes[i]; j++) {
-            inputs[i][j] = std::rand() % 100 + 1; // 1 to 100
-        }
-    }
+        int input_size = std::rand() % 971 + 30; // 30 to 1000
 
-    long ins_cmps = 0;
-    long merge_cmps = 0;
-    long heap_cmps = 0;
-    long quick_cmps = 0;
+        std::cout << "Run #" << i+1 << '\n';
+        std::cout << "Size: " << input_size << '\n';
 
-    for (int i = 0; i < 10; i++) {
-        // duplicate input values for each sort
-        int *A = new int[input_sizes[i]];
-        int *B = new int[input_sizes[i]];
-        int *C = new int[input_sizes[i]];
-        int *D = new int[input_sizes[i]];
+        for (int j = 0; j < RUN_SIZE; j++) {
+            int *A = new int[input_size];
 
-        for (int j = 0; j < input_sizes[i]; j++) {
-            A[j] = inputs[i][j];
-            B[j] = inputs[i][j];
-            C[j] = inputs[i][j];
-            D[j] = inputs[i][j];
+            generate_data(A, input_size);
+
+            avg_ins += insertion_sort_copy(A, input_size);
+            avg_merge += merge_sort_copy(A, input_size);
+            avg_heap += heap_sort_copy(A, input_size);
+            avg_quick += quick_sort_copy(A, input_size);
         }
 
-        ins_cmps   = insertion_sort(A, input_sizes[i]);
-        merge_cmps = merge_sort(B, input_sizes[i]);
-        heap_cmps  = heap_sort(C, input_sizes[i]);
-        quick_cmps = quick_sort(D, input_sizes[i]);
+        std::cout << "  Average insertion sort comparisons: " << ((double)avg_ins)/RUN_SIZE << '\n';
+        std::cout << "  Average merge sort comparisons: " << ((double)avg_merge)/RUN_SIZE << '\n';
+        std::cout << "  Average heap sort comparisons: " << ((double)avg_heap)/RUN_SIZE << '\n';
+        std::cout << "  Average quick sort comparisons: " << ((double)avg_quick)/RUN_SIZE << '\n';
 
-        csv << input_sizes[i]    << ','
-            << ins_cmps          << ','
-            << merge_cmps        << ','
-            << heap_cmps         << ','
-            << quick_cmps        << ','
-            << input_sizes[i]*std::log2(input_sizes[i]) << '\n';
+        csv << input_size                   << ','
+            << ((double)avg_ins)/RUN_SIZE   << ','
+            << ((double)avg_merge)/RUN_SIZE << ','
+            << ((double)avg_heap)/RUN_SIZE  << ','
+            << ((double)avg_quick)/RUN_SIZE << ','
+            << input_size * std::log2(input_size) << '\n';
     }
 
     return 0;
